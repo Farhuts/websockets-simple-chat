723dd10822f4d6da71e6b7d66eb73893026fd656
Fenomamy R <anja1catus@gmail.com>
2022-02-15 21:31:54 +0300

    fixing the bug by adding more parameters to the socket io instance/ doing some update on the node packages

--- index.js
+++ index.js
@@ -1,32 +1,40 @@
-const express = require('express')
-const socket = require('socket.io')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-const server = app.listen(8080, function(){
-  console.log("Listening on port 8080")
-})
+const server = app.listen(8080, function () {
+  console.log("Listening on port 8080");
+});
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:8042",
+    methods: ["GET", "POST"],
+    transports: ["websocket", "polling"],
+    credentials: true,
+  },
+  allowEIO3: true,
+});
 
 // Socket setup
 // Listens for the server
-const io = socket(server)
 
 io.on("connection", (socket) => {
-  console.log("made socket connection", socket.id)
+  console.log("made socket connection", socket.id);
 
-// Listens for the message from the client and emits (sends) it to the clients
-  socket.on("message", (data)=>{
-    io.sockets.emit("message", data)
-  })
-
-// Listens for the typing from the client and broadcasts it to all the clients, except the sender
-  socket.on('typing', function(data){
-      socket.broadcast.emit('typing', data);
+  // Listens for the message from the client and emits (sends) it to the clients
+  socket.on("message", (data) => {
+    io.sockets.emit("message", data);
   });
 
-// Listens to when someone leaves the chat
-  socket.on('disconnect', (socket)=>{
-    console.log("disconnected", socket.id)
-  })
-})
+  // Listens for the typing from the client and broadcasts it to all the clients, except the sender
+  socket.on("typing", function (data) {
+    socket.broadcast.emit("typing", data);
+  });
+
+  // Listens to when someone leaves the chat
+  socket.on("disconnect", (socket) => {
+    console.log("disconnected", socket.id);
+  });
+});
